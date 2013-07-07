68bb07871470656bd27724b0731ca619f2ae25d1
Ben Hsu <benhsu@gmail.com>
2013-07-07 13:13:09 -0400

    unify to 1 file to prevent symbol not found errors

--- all.cc
+++ all.cc
@@ -1,6 +1,8 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string>
+// what is the difference between import and include?
+#import <vector>
 
 enum Token {
     tok_eof=-1,
@@ -57,4 +59,63 @@ static int gettok() {
     int ThisChar = LastChar;
     LastChar = getchar();
     return ThisChar;
+}
+
+
+class ExprAst {
+ public:
+    virtual ~ExprAst() {};
+};
+
+class NumberExprAst:ExprAst {
+private:
+    double Val;
+public:
+    NumberExprAst(double Val):Val(Val) {}
+};
+
+class VariableExprAst:ExprAst {
+private:
+    std::string Name;
+public:
+    VariableExprAst(const std::string &name) : Name(name) {}
+};
+
+class BinaryExprAst:ExprAst {
+private:
+    char Op;
+    /* what do the * mean here?*/
+    ExprAst *LHS, *RHS;
+public:
+    BinaryExprAst(char op, ExprAst *lhs, ExprAst *rhs) : Op(op), LHS(lhs), RHS(rhs) {}
+};
+
+class CallExprAst:ExprAst {
+private:
+    std::string Callee;
+    std::vector<ExprAst*> Args;
+public:
+    CallExprAst(const std::string Callee, std::vector<ExprAst*> Args) : Callee(Callee), Args(Args) {}
+};
+
+class PrototypeAst /* note this does not inherit from ExprAst */ {
+    std::string Name;
+    std::vector<std::string> Args;
+public:
+    PrototypeAst(const std::string &Name, const std::vector<std::string> &args) : Name(Name), Args(args) {}
+};
+
+class FunctionAst { 
+    /* a function has a prototype and a body! this is getting cool!!!! */
+    PrototypeAst *Proto;
+    ExprAst *Body;
+public:
+    /* QUESTION: what do * and & mean in the arg list. and why are some things const and others not? */
+    FunctionAst(PrototypeAst *Proto, ExprAst *body) : Proto(Proto), Body(body) {}
+};
+    
+static int CurTok;
+
+static int getNextToken() {
+    return CurTok = gettok();
 }
