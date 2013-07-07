3f16aeaaef8fa98ca4b0613372f23cc429b1345d
Ben Hsu <benhsu@gmail.com>
2013-07-07 13:43:26 -0400

    at section 2.5

--- all.cc
+++ all.cc
@@ -67,21 +67,21 @@ class ExprAST {
     virtual ~ExprAST() {};
 };
 
-class NumberExprAST:ExprAST {
+class NumberExprAST: public ExprAST {
 private:
     double Val;
 public:
     NumberExprAST(double Val):Val(Val) {}
 };
 
-class VariableExprAST:ExprAST {
+class VariableExprAST: public ExprAST {
 private:
     std::string Name;
 public:
     VariableExprAST(const std::string &name) : Name(name) {}
 };
 
-class BinaryExprAST:ExprAST {
+class BinaryExprAST: public ExprAST {
 private:
     char Op;
     /* what do the * mean here?*/
@@ -90,7 +90,7 @@ public:
     BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs) : Op(op), LHS(lhs), RHS(rhs) {}
 };
 
-class CallExprAST:ExprAST {
+class CallExprAST: public ExprAST {
 private:
     std::string Callee;
     std::vector<ExprAST*> Args;
@@ -120,8 +120,70 @@ static int getNextToken() {
     return CurTok = gettok();
 }
 
-ExprAST *error(const char *Str) {
+/* error handling is always useful! */
+ExprAST *Error(const char *Str) {
     fprintf(stderr, "Error %s\n", Str);
     return 0;
 }
 
+PrototypeAST *ErrorP(const char *Str) { Error(Str); return 0;}
+
+/* needs different name otherwise compiler wont be able to tell which one was called*/
+FunctionAST *ErrorF(const char *Str) {Error(Str); return 0;}
+
+// called when current token is a number. expects NumVal to be populated by gettok()
+static ExprAST *ParseNumberExpr() {
+    ExprAST *Result = new NumberExprAST(NumVal); /* numval is global */
+    getNextToken(); // munch 
+    return Result;
+}
+
+// parenexpr = '(' + expr + ')'
+static ExprAST *ParseParenExpr() {
+    getNextToken(); // eat the '('
+    ExprAST *V = ParseExpression(); // to be populated, recursive descent!
+    if (!V) return 0; // error? or ()
+    if (CurTok!=')') {
+        return Error("Expected ')'");
+    }
+    getNextToken(); // eat the ')'
+    return V;
+}
+
+// we have one function to resolve a variable ref AND a function call?
+// could this be because jmc is a genius and realized they are both s-expressions? probably not
+
+static ExprAST *ParseIdentifierExpr() {
+    std::string Idname = IdentifierStr; // this is populated by gettok()
+    getNextToken(); // eat the identifier
+    if (CurTok != '(') { // use look ahead to check next token!
+        // simple identifier name
+        return new VariableExprAST(Idname);
+    }
+    getNextToken(); // eat '('
+    std::vector<ExprAST*> Args;
+    if(CurTok!=')') /* seems clumsy */ {
+        while(1) {
+            ExprAST *Arg = ParseExpression(); // keep in mind each argument can be a function call!
+            if(!Arg) return 0;
+            Args.push_back(Arg);
+            if(CurTok==')') break; /* WTF is this shit? */
+            if(CurTok!=',') return Error("Expected ')' or ','");
+            getNextToken();
+        }
+    }
+    getNextToken(); //eat the ')'
+    return new CallExprAST(Idname, Args);
+}
+
+// a "primary expression" is a identifier (lval), a number (rval), or a paren expression??? 
+// this will become clear later. have faith in the tutorial
+
+static ExprAST *ParsePrimary() {
+    switch(CurTok) {
+    case tok_identifier: return ParseIdentifierExpr();
+    case tok_number: return ParseNumberExpr();
+    case '(': return ParseParenExpr();
+    default: return Error("I dont know what this token is");
+    }
+}
